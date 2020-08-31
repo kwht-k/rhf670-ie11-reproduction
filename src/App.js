b5a2967f5252269b837f95ef7790d7d20841d4fc
kuwahata <kengo.kuwahata.gu@nttcom.co.jp>
2020-08-31 14:11:48 +0900

    Add reproduction code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useForm } from "react-hook-form/dist/index.ie11";
 
 function App() {
+  const { register, handleSubmit, formState } = useForm({
+    mode: "onBlur",
+  });
+  const { isValid } = formState;
+
+  const onSubmit = (data) => {
+    //alert(JSON.stringify(data));
+    console.log(data);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <input
+        type="text"
+        ref={register({
+          required: true,
+        })}
+      />
+      <input type="submit" name="name" disabled={!isValid} value="Submit" />
+    </form>
   );
 }
 
